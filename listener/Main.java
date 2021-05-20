c3f1ff41112fa52df3d431c87fc813a922d36abc
justinprez <prezjustin@gmail.com>
2021-05-20 19:39:30 -0400

    Create Main.java

---
+++ listener/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        KeyListener.SuppressKeyListenerErrorLogs();
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
+
+        GlobalScreen.addNativeKeyListener(new KeyListener());
+    }
+}
