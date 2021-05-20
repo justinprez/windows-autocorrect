91d2d647466f39b64c35e006ae225b5b73e50113
justinprez <prezjustin@gmail.com>
2021-05-20 19:39:27 -0400

    Create KeyListener.java

---
+++ listener/KeyListener.java
@@ -0,0 +1,43 @@
+package com.company;
+import com.company.WordProcessor;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class KeyListener implements NativeKeyListener {
+    boolean m_HasInitialized = false;
+
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        if ( ! m_HasInitialized ){
+            m_HasInitialized = true;
+        }
+        WordProcessor.Process( e );
+        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            try {
+                GlobalScreen.unregisterNativeHook();
+            } catch (NativeHookException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {}
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+    }
+
+    public static void SuppressKeyListenerErrorLogs(){
+        Logger logger = Logger.getLogger( GlobalScreen.class.getPackage().getName() );
+        logger.setLevel(Level.OFF);
+
+        Handler[] handlers = Logger.getLogger("").getHandlers();
+        for (int i = 0; i < handlers.length; i++) {
+            handlers[i].setLevel(Level.OFF);
+        }
+    }
+}
